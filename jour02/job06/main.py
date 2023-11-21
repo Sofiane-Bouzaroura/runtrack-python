6d06170a14c9d676ccb82cd26408ef84e77b80d8
Sofiane-Bouzaroura <sofiane.bouzaroura@laplateforme.io>
2023-11-21 19:42:01 +0100

    modifier

--- jour02/job06/main.py
+++ jour02/job06/main.py
@@ -1,37 +1,13 @@
-# for n in range (1,1000):
-#     if n > 1:
-#         for i in range(2,n):
-#             if n
+max=1000
+for nomber in range(2,max +1):
+    est_premier=True
 
-# def est_premier(nomber):
-#     if nomber < 2:
-#         return False
+    if nomber < 2:
+        est_premier=False
 
-# for i in range(2, int(nomber**0.5) + 1):
-#     if (nomber % i == 0):    
-#         return False
-            
-# return True
-
-# def afficher_nombres_premiers(maximum):
-#     for nomber in range(2, maximum + 1):
-#          if est_premier(nomber):
-#               print(nomber, end=' ')
-#               afficher_nombres_premiers(1000)
-
-def est_premier(nombre):
-    if nombre < 2:
-        return False
-
-    for i in range(2, int(nombre**0.5) + 1):
-        if nombre % i == 0:
-            return False
-
-    return True
-
-def afficher_nombres_premiers(maximum):
-    for nombre in range(2, maximum + 1):
-        if est_premier(nombre):
-            print(nombre, end=' ')
-
-afficher_nombres_premiers(1000)
+    for n in range(2, int(nomber**0.5) +1):
+        if nomber % n == 0:
+            est_premier = False
+            break
+    if est_premier:
+        print(nomber, end=" ")        
