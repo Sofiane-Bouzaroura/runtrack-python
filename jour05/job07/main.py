daf682c8f7a1d66e61e6d4e3fb696d8f20e5d64f
Sofiane-Bouzaroura <sofiane.bouzaroura@laplateforme.io>
2023-11-27 19:07:22 +0100

    modif

--- jour05/job07/main.py
+++ jour05/job07/main.py
@@ -2,11 +2,11 @@ def evaluation_note():
     note = float(input("Entrez la note sur 100 : "))
 
     if note >= 40:
-        prochain_multiple_de_5 = ((note // 5) + 1) * 5
-        difference = prochain_multiple_de_5 - note
+        multiple_de_5 = ((note // 5) + 1) * 5
+        difference = multiple_de_5 - note
 
         if difference < 3:
-            note_arrondie = prochain_multiple_de_5
+            note_arrondie = multiple_de_5
         else:
             note_arrondie = note
 
